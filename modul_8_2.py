53811971bdb84f557f6eeafb25d573acd2ae3376
Abeille1990 <tatiana.tyukova@gmail.com>
2024-09-07 14:34:49 +0300

    Add files via upload

---
+++ modul_8_2.py
@@ -0,0 +1,42 @@
+def personal_sum(numbers: list):
+    result = 0
+    incorrect_data = 0
+
+    for i in numbers:
+        try:
+            result += i
+        except:
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
+            incorrect_data += 1
+
+    return result, incorrect_data
+
+
+def calculate_average(numbers: list):
+    try:
+        sum_ = personal_sum(numbers)
+        # devider = (len(numbers) - personal_sum(numbers)[1])
+        # average = sum_ / devider
+        return sum_[0]/(len(numbers)-sum_[1])
+
+    except ZeroDivisionError as exc:
+        print(f'Tак как входящие данные даны в'
+              f' неверном формте - делитель для среднего = 0,'
+              f'а на ноль делить нельзя! /{exc}')
+        return 0
+
+    except TypeError as exc:
+        print(f'В numbers записан некорректный тип данных')
+
+
+
+
+# print(personal_sum("1, 2, 3"))
+# print(personal_sum([1, "Строка", 3, "Ещё Строка"]))
+# print(personal_sum(567))
+# print(personal_sum([42, 15, 36, 13]))
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
